93d4cb3333b90b13e5e4296fc220b4a2dc597709
Aaron Garrett <aaron.lee.garrett@gmail.com>
2018-09-10 02:41:17 +0000

    Add initial source files

---
+++ src/test/java/edu/wofford/GuessMyNumberTest.java
@@ -0,0 +1,15 @@
+package edu.wofford;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+
+public class GuessMyNumberTest {
+
+    @Test
+    public void testSomething() {
+        
+    }
+   
+}
