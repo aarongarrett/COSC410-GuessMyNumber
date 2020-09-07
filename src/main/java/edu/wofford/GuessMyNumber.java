69b636208712127ad7c199802d6740536837115f
Aaron Garrett <aaron.lee.garrett@gmail.com>
2020-09-07 14:28:10 -0400

    first steps

--- src/main/java/edu/wofford/GuessMyNumber.java
+++ src/main/java/edu/wofford/GuessMyNumber.java
@@ -2,5 +2,21 @@ package edu.wofford;
 
 
 public class GuessMyNumber {
-    
+    public enum Result { LOW, HIGH, EQUAL };
+
+    public GuessMyNumber(int number) {
+        target = number;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public Result guess(int g) {
+        if (g < target) return Result.LOW;
+        else if (g > target) return Result.HIGH;
+        else return Result.EQUAL;
+    }
+
+    private int target;
 }
--- src/test/java/edu/wofford/GuessMyNumberTest.java
+++ src/test/java/edu/wofford/GuessMyNumberTest.java
@@ -8,8 +8,16 @@ import static org.hamcrest.CoreMatchers.*;
 public class GuessMyNumberTest {
 
     @Test
-    public void testSomething() {
-        
+    public void testConstructor() {
+        GuessMyNumber g = new GuessMyNumber(42);
+        assertThat(g.getTarget(), is(42));
+    }
+
+    @Test
+    public void testGuessTooLow() {
+        GuessMyNumber g = new GuessMyNumber(37);
+        GuessMyNumber.Result result = g.guess(15);
+        assertThat(result, is(GuessMyNumber.Result.LOW));
     }
    
 }
